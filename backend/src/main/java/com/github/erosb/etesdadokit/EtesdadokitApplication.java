1cfbe1029090131b9aa3e43d6ef96a3ed9bcad74
Zoltan Polgar <pozo@gmx.com>
2020-04-03 17:03:30 +0200

    Backend base

---
+++ backend/src/main/java/com/github/erosb/etesdadokit/EtesdadokitApplication.java
@@ -0,0 +1,13 @@
+package com.github.erosb.etesdadokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtesdadokitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtesdadokitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/erosb/etesdadokit/EtesdadokitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.erosb.etesdadokit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtesdadokitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
