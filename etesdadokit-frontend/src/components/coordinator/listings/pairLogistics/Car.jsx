2db1b68daf84eefe129f23640f7e7426b2e74c34
nhajdu <hajdu.nora@e-vista.hu>
2020-04-04 18:16:57 +0200

    Basic food list on logistics

--- etesdadokit-frontend/src/components/coordinator/listings/pairLogistics/Car.jsx
+++ etesdadokit-frontend/src/components/coordinator/listings/pairLogistics/Car.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 
 const Car = (details) => {
     console.log(details)
+    const { details: { name } } = details
     return (
         <div>
-            Fuvar
+            {JSON.stringify(details)}
+            <div>{name}</div>
         </div>
     );
 }
